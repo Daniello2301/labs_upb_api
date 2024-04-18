5c96b4fea1a00307242598d35f29c838d362285c
danielloUPB <daniel.lopera@upb.edu.co>
2024-04-18 09:22:10 -0500

    docs: change location files to better read

--- src/main/java/co/edu/upb/labs_upb/dto/AuthenticationRequest.java
+++ src/main/java/co/edu/upb/labs_upb/dto/AuthenticationRequest.java
@@ -1,4 +1,4 @@
-package co.edu.upb.labs_upb.model;
+package co.edu.upb.labs_upb.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- src/main/java/co/edu/upb/labs_upb/dto/AuthenticationResponse.java
+++ src/main/java/co/edu/upb/labs_upb/dto/AuthenticationResponse.java
@@ -1,4 +1,4 @@
-package co.edu.upb.labs_upb.model;
+package co.edu.upb.labs_upb.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
