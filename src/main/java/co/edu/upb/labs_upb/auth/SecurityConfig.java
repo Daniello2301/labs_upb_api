04d87ed3c8d1497fa4ba4e43735f56d70fc3085f
danielloUPB <daniel.lopera@upb.edu.co>
2024-04-22 11:22:41 -0500

    change: add public equipments endpoints to security config

--- src/main/java/co/edu/upb/labs_upb/auth/SecurityConfig.java
+++ src/main/java/co/edu/upb/labs_upb/auth/SecurityConfig.java
@@ -56,6 +56,8 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
             "/auth/**",
             "/prestamos/create",
+            "/activos/enable/**",
+            "/activos/pagination",
             "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
